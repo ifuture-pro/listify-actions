18e418a5619c4eed770bacd3861d3a233e2c8bb2
ifuture-pro <strongrobot@163.com>
2019-12-03 22:35:11 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 
 echo ${INPUT_LISTIFY}
 
-COMMIT_MESSAGE = $(curl -s https://api.github.com/repos/ifuture-pro/listify-actions/commits/$GITHUB_SHA |grep jq '.commit.message') && \
+COMMIT_MESSAGE="$(curl -s https://api.github.com/repos/ifuture-pro/listify-actions/commits/$GITHUB_SHA | jq '.commit.message')" && \
 
 if [ "$COMMIT_MESSAGE" -eq "listify Auto" ] ; then
   echo "listify auto push. Ignore" && \

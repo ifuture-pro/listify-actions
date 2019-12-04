a70ac07a6286cabe7aa8f9dc2930ef4738e2d7b6
ifuture-pro <strongrobot@163.com>
2019-12-04 11:58:10 +0800

    BUILD

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 
 
 echo ${INPUT_LISTIFY}
+INPUT_BRANCH=${INPUT_BRANCH:-master}
+INPUT_FORCE=${INPUT_FORCE:-false}
+REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 AUTO_COMMIT_MSG="listify Auto"
-COMMIT_MESSAGE="$(curl -s https://api.github.com/repos/ifuture-pro/listify-actions/commits/$GITHUB_SHA | jq '.commit.message' | sed 's/\"//g')"
+COMMIT_MESSAGE="$(curl -s https://api.github.com/repos/${GITHUB_ACTOR}/${GITHUB_REPOSITORY}/commits/$GITHUB_SHA | jq '.commit.message' | sed 's/\"//g')"
 
 echo "latest commit is : ${COMMIT_MESSAGE}"
 
@@ -13,6 +16,15 @@ if [[ "$COMMIT_MESSAGE" == "$AUTO_COMMIT_MSG" ]] ; then
     echo "listify auto push. Ignore"
     exit 0
 fi
+
+[ -z "${INPUT_GITHUB_TOKEN}" ] && {
+    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
+
+REMOTE_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+
+echo $REMOTE_REPO
 
 listify ./
 
@@ -25,4 +37,5 @@ fi
 git config --list
 git add .
 git commit -m "${AUTO_COMMIT_MSG}"
-git push
+
+git push "${REMOTE_REPO}" HEAD:${INPUT_BRANCH}

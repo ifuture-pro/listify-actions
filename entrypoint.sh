61d82ed42ee8459a05d244e876517ef6a6cee68d
ifuture-pro <strongrobot@163.com>
2019-12-04 10:19:34 +0800

    build

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,22 +4,24 @@ set -e
 
 
 echo ${INPUT_LISTIFY}
+AUTO_COMMIT_MSG="listify Auto"
+COMMIT_MESSAGE="$(curl -s https://api.github.com/repos/ifuture-pro/listify-actions/commits/$GITHUB_SHA | jq '.commit.message' | sed 's/\"//g')"
 
-COMMIT_MESSAGE="$(curl -s https://api.github.com/repos/ifuture-pro/listify-actions/commits/$GITHUB_SHA | jq '.commit.message')"
+echo "latest commit is : ${COMMIT_MESSAGE}"
 
-if [ "$COMMIT_MESSAGE" -eq "listify Auto" ] ; then
-    echo "listify auto push. Ignore" && \
+if [[ "$COMMIT_MESSAGE" == "$AUTO_COMMIT_MSG" ]] ; then
+    echo "listify auto push. Ignore"
     exit 0
-fi && \
+fi
 
-listify ./ && \
+listify ./
 
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 if [ -z "$(git status --porcelain)" ]; then
-    echo "Nothing to commit" && \
+    echo "Nothing to commit"
     exit 0
-fi && \
-git add . && \
-git commit -m 'listify Auto' && \
-git push && \
+fi
+git add .
+git commit -m "${AUTO_COMMIT_MSG}"
+git push
